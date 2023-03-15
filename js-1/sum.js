e49edef90ffd3f407ff00b79b34384830046c154
Diego Mauricio Ortiz <dmortizu@gmail.com>
2023-03-15 00:07:37 -0500

    Ejercicio sum resuelto

--- js-1/sum.js
+++ js-1/sum.js
@@ -1,9 +1,14 @@
 // escribe la función suma acá
-function sum(a) {
-    return a + 1
-}   
+function sum(array) {
+    let sumaAcum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sumaAcum += array[i];
+    }
+    return `Suma de arreglos: ${sumaAcum}`;
+  
+}
 
 // código de prueba
-console.log(sum(4)) // 1 + 2 + 3 + 4 = 10
-console.log(sum(10)) // 55
-console.log(sum(15)) // 120
+console.log(sum([1,2,3,4])) // 1 + 2 + 3 + 4 = 10
+console.log(sum([10,25,15,5])) // 55
+console.log(sum([15,25,35,25,20])) // 120
